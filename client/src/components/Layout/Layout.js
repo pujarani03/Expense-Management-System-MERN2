511e0ffd69d1709876466cfad0aac676f4505207
pujarani03 <138789765+pujarani03@users.noreply.github.com>
2025-02-23 06:52:59 +0530

    Initial commit

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Children } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({ Children }) => {
+    return (
+        <>
+           <Header/>
+           <div className="content">
+             {Children}
+           </div>
+           <Footer/>
+        </>
+    );
+};
+
+export default Layout;
